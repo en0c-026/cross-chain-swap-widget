46174b0115c69243335738d156b20457edf14603
en0c-026 <bitup.games@gmail.com>
2022-04-21 18:58:56 +0200

    add basic constants

---
+++ src/constants.ts
@@ -0,0 +1,87 @@
+
+export const ERC20Abi = [
+  // Read-Only Functions
+  "function balanceOf(address owner) view returns (uint256)",
+
+  // Authenticated Functions
+  "function approve(address spender, uint amount) returns (boolean)",
+  // Events
+  "event Approval(address indexed owner, address indexed spender, uint value)"
+];
+
+export const theme = {
+  global: {
+    font: {
+      family: "'Metric', Arial, sans-serif",
+      face: `
+        @font-face {
+          font-family: "Metric";
+          src: url("https://d3hq6blov2iije.cloudfront.net/fonts/HPEXS-Metric-Fonts/MetricHPEXS-Regular.woff2") format('woff2'),
+               url("https://d3hq6blov2iije.cloudfront.net/fonts/HPEXS-Metric-Fonts/MetricHPEXS-Regular.woff") format('woff');
+        }
+        @font-face {
+          font-family: "Metric";
+          src: url("https://d3hq6blov2iije.cloudfront.net/fonts/HPEXS-Metric-Fonts/MetricHPEXS-Bold.woff2") format('woff2'),
+               url("https://d3hq6blov2iije.cloudfront.net/fonts/HPEXS-Metric-Fonts/MetricHPEXS-Bold.woff") format('woff');
+          font-weight: 700;
+        }
+        @font-face {
+          font-family: "Metric";
+          src: url("https://d3hq6blov2iije.cloudfront.net/fonts/HPEXS-Metric-Fonts/MetricHPEXSSemibold-Regular.woff2") format('woff2'),
+               url("https://d3hq6blov2iije.cloudfront.net/fonts/HPEXS-Metric-Fonts/MetricHPEXSSemibold-Regular.woff") format('woff');
+          font-weight: 600;
+        }
+        @font-face {
+          font-family: "Metric";
+          src: url("https://d3hq6blov2iije.cloudfront.net/fonts/HPEXS-Metric-Fonts/MetricHPEXSMedium-Regular.woff2") format('woff2'),
+               url("https://d3hq6blov2iije.cloudfront.net/fonts/HPEXS-Metric-Fonts/MetricHPEXSMedium-Regular.woff") format('woff');
+          font-weight: 500;
+        }
+        @font-face {
+          font-family: "Metric";
+          src: url("https://d3hq6blov2iije.cloudfront.net/fonts/HPEXS-Metric-Fonts/MetricHPEXSLight-Regular.woff2") format('woff2'),
+               url("https://d3hq6blov2iije.cloudfront.net/fonts/HPEXS-Metric-Fonts/MetricHPEXSLight-Regular.woff") format('woff');
+          font-weight: 100;
+        }`,
+    },
+    control: {
+      border: {
+        width: '0px'
+      }
+    },
+    focus: {
+      outline: {
+        size: '0px'
+      }
+    }
+  },
+  layer: {
+    border: {
+      radius: 'medium',
+      intelligentRounding: true,
+    },
+  },
+  checkBox: {
+    size: '18px'
+  },
+  radioButton: {
+    size: '18px'
+  },
+  select: {
+    options: {
+      container: {
+        size: '5px'
+      }
+    }
+  },
+  tip: {
+    content: {
+      background: "dark-4",
+      elevation: "none",
+      margin: "none",
+      pad: "small",
+      round: "small"
+    }
+  },
+
+};
