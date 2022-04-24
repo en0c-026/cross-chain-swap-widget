afbaef86b9ffa1bdcdb3337f0a3a08b19deb62bf
en0c-026 <bitup.games@gmail.com>
2022-04-24 18:10:58 +0200

    add chainsSupported and switchChain function

--- src/context/WalletProvider.tsx
+++ src/context/WalletProvider.tsx
@@ -4,37 +4,101 @@ import { JsonRpcSigner, Web3Provider } from "@ethersproject/providers";
 import Web3Modal from "web3modal";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import { useConfig } from ".";
+import { ChainConfig } from "../models";
+import { hexlify } from "@ethersproject/bytes";
 
 interface IWalletProviderContext {
   accounts: string[] | undefined;
   connect: Web3Modal["connect"];
-  currentChainId: number | undefined;
+  chainsSupported: ChainConfig[];
+  currentChain: ChainConfig | undefined;
   disconnect: Web3Modal["clearCachedProvider"];
   isLoggedIn: boolean;
   rawEthereumProvider: undefined | any;
   signer: JsonRpcSigner | undefined;
   walletProvider: Web3Provider | undefined;
   web3Modal: Web3Modal | undefined;
+  switchChain: (targetChain: ChainConfig) => Promise<void>;
 }
 
 const WalletProviderContext = createContext<IWalletProviderContext | null>(null);
 
 export default function WalletProviderProvider({ children }: { children: ComponentChildren }) {
 
-  const { infuraId } = useConfig();
+  const { bcnmyApiKeys, infuraId, rpcUrls } = useConfig();
   const [accounts, setAccounts] = useState<string[]>();
-  const [currentChainId, setCurrentChainId] = useState<number>();
+  const [currentChain, setCurrentChain] = useState<ChainConfig>();
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
   const [rawEthereumProvider, setRawEthereumProvider] = useState<any>();
   const [signer, setSigner] = useState<JsonRpcSigner>();
   const [walletProvider, setWalletProvider] = useState<undefined | Web3Provider>();
   const [web3Modal, setWeb3Modal] = useState<undefined | Web3Modal>();
 
+  const chainsSupported: ChainConfig[] = [
+    {
+      name: "Avalanche",
+      image: 'https://app.1inch.io/assets/images/network-logos/avalanche.svg',
+      subText: "Avalanche mainnet",
+      chainId: 43114,
+      rpcUrl: "https://api.avax.network/ext/bc/C/rpc",
+      currency: "AVAX",
+      nativeToken: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+      nativeDecimal: 18,
+      nativeFaucetURL: "",
+      biconomy: {
+        enable: false,
+        apiKey: bcnmyApiKeys.avalanche,
+      },
+      assetSentTopicId: "0xec1dcc5633614eade4a5730f51adc7444a5103a8477965a32f2e886f5b20f694",
+      networkAgnosticTransfer: false, // Set this to enable network agnostic gasless transactions
+      graphURL: "https://api.thegraph.com/subgraphs/name/divyan73/hyphen-avalanche",
+      explorerUrl: "https://snowtrace.io"
+    },
+    {
+      name: "Ethereum",
+      image: 'https://app.1inch.io/assets/images/network-logos/ethereum.svg',
+      subText: "Ethereum Mainnet",
+      chainId: 1,
+      rpcUrl: rpcUrls.ethereum.mainnet,
+      currency: "ETH",
+      nativeToken: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+      nativeDecimal: 18,
+      nativeFaucetURL: "",
+      assetSentTopicId: "0xec1dcc5633614eade4a5730f51adc7444a5103a8477965a32f2e886f5b20f694",
+      biconomy: {
+        enable: false,
+        apiKey: bcnmyApiKeys.ethereum,
+      },
+      graphURL: "https://api.thegraph.com/subgraphs/name/divyan73/hyphenethereumv2",
+      networkAgnosticTransfer: false,
+      explorerUrl: "https://etherscan.io/",
+    },
+    {
+      name: "Polygon",
+      image: 'https://app.1inch.io/assets/images/network-logos/polygon.svg',
+      subText: "Polygon Mainnet",
+      chainId: 137,
+      rpcUrl: "https://polygon-rpc.com/",
+      currency: "MATIC",
+      nativeToken: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+      nativeDecimal: 18,
+      nativeFaucetURL: "",
+      biconomy: {
+        enable: true,
+        apiKey: bcnmyApiKeys.polygon,
+      },
+      assetSentTopicId: "0xec1dcc5633614eade4a5730f51adc7444a5103a8477965a32f2e886f5b20f694",
+      networkAgnosticTransfer: true,
+      graphURL: "https://api.thegraph.com/subgraphs/name/divyan73/hyphenpolygonv2",
+      explorerUrl: "https://polygonscan.com",
+    }
+  ]
+
   useEffect(() => {
     if (
       rawEthereumProvider &&
       walletProvider &&
-      currentChainId &&
+      currentChain &&
       accounts &&
       accounts[0] &&
       accounts[0].length > 0
@@ -43,7 +107,7 @@ export default function WalletProviderProvider({ children }: { children: Compone
     } else {
       setIsLoggedIn(false);
     }
-  }, [rawEthereumProvider, walletProvider, currentChainId, accounts]);
+  }, [rawEthereumProvider, walletProvider, currentChain, accounts]);
 
   useEffect(() => {
     if (!walletProvider) return;
@@ -73,9 +137,13 @@ export default function WalletProviderProvider({ children }: { children: Compone
     if (!walletProvider) return;
     (async () => {
       let { chainId } = await walletProvider.getNetwork();
+      chainsSupported.map(chain => {
+        if (chain.chainId === chainId) {
+          setCurrentChain(chain)
+        }
+      })
       let accounts = await walletProvider.listAccounts();
       setAccounts(accounts.map((a) => a.toLowerCase()));
-      setCurrentChainId(chainId);
     })();
   }, [walletProvider]);
 
@@ -99,16 +167,29 @@ export default function WalletProviderProvider({ children }: { children: Compone
     function handleChainChanged(chainId: string | number) {
       console.log("chainChanged!");
       if (typeof chainId === "string") {
-        setCurrentChainId(Number.parseInt(chainId));
+        let chainIdParsed = Number.parseInt(chainId)
+        chainsSupported.map(chain => {
+          if (chain.chainId === chainIdParsed) {
+            setCurrentChain(chain)
+          }
+        })
       } else {
-        setCurrentChainId(chainId);
+        chainsSupported.map(chain => {
+          if (chain.chainId === chainId) {
+            setCurrentChain(chain)
+          }
+        })
       }
       reinit(rawEthereumProvider);
     }
 
     function handleConnect(info: { chainId: number }) {
       console.log("connect!");
-      setCurrentChainId(info.chainId);
+      chainsSupported.map(chain => {
+        if (chain.chainId === info.chainId) {
+          setCurrentChain(chain)
+        }
+      })
       reinit(rawEthereumProvider);
     }
 
@@ -161,18 +242,54 @@ export default function WalletProviderProvider({ children }: { children: Compone
     setWalletProvider(undefined);
   }, [web3Modal]);
 
+  const switchChain = useCallback(async (targetChain: ChainConfig) => {
+    const chainIdHex = hexlify(targetChain.chainId);
+    try {
+      await rawEthereumProvider.request({
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: chainIdHex }]
+      })
+    } catch (error) {
+      try {
+        if (error.code === 4902) {
+          await rawEthereumProvider.request({
+            method: 'wallet_addEthereumChain',
+            params: [{
+              chainId: chainIdHex,
+              blockExplorerUrls: [targetChain.explorerUrl],
+              chainName: targetChain.name,
+              iconUrls: [targetChain.image],
+              nativeCurrency: {
+                name: targetChain.name,
+                symbol: targetChain.currency,
+                decimals: targetChain.nativeDecimal,
+              },
+              rpcUrls: [targetChain.rpcUrl],
+            }],
+          })
+        } else {
+          console.log(error)
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }, [rawEthereumProvider])
+
   return (
     <WalletProviderContext.Provider
       value={{
         accounts,
         connect,
-        currentChainId,
+        chainsSupported,
+        currentChain,
         disconnect,
         isLoggedIn,
         rawEthereumProvider,
         signer,
         walletProvider,
         web3Modal,
+        switchChain
       }}
     >
       {children}
