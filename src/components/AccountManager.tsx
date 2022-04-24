16591bf7435913aa4ab0613abf1c0ea04d723ed2
en0c-026 <bitup.games@gmail.com>
2022-04-24 18:14:02 +0200

    update styles

--- src/components/AccountManager.tsx
+++ src/components/AccountManager.tsx
@@ -3,37 +3,61 @@ import Container from './Container'
 import { Button, Text } from 'grommet'
 import { useWalletProvider } from '../context/WalletProvider'
 
+const ButtonTitle = ({title}: { title: string}) => {
+  return (
+    <Container style={{
+      background: 'c3',
+      round: 'small',
+      pad: { horizontal: 'small', vertical: 'xxsmall'},
+      direction: 'row'
+    }}>
+      <Text size='small'>
+        {title}
+      </Text>
+    </Container>
+  )
+}
+
+
 export default function AccountManager() {
   const { connect, disconnect, isLoggedIn, accounts } = useWalletProvider()!;
+
   return (
     <Container
       style={{
         direction: 'row',
-        gap: 'small',
-        align: 'center'
+        gap: 'xxsmall',
+        align: 'center',
       }}
     >
-      <Text>
+      <Text
+      size='small'
+      >
         {isLoggedIn &&
           accounts &&
-          (`${accounts[0].substring(0, 6)}...${accounts[0].substring(accounts[0].length - 6)}`)
+          (`${accounts[0].substring(0, 6)}...${accounts[0].substring(accounts[0].length - 4)}`)
         }
       </Text>
       {isLoggedIn ?
         (
           <Button
+            size='small'
             onClick={() => {
               disconnect()
               window.location.reload()
             }}
-            label="Disconnect"
+            label={<ButtonTitle title={"Disconnect"} />}
+            plain
           />
         ) : (
-          <Button onClick={() => connect()}
-            label="Connect"
+          <Button
+            size='small'
+            onClick={() => connect()}
+            label={<ButtonTitle title={"Connect"} />}
+            plain
           />
         )
-
+          
       }
     </Container>
   )
